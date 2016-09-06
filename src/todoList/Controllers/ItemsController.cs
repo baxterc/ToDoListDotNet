c21bec126aa71f3f31f5d87087daa5d5e79c3f3f
Bhawani Parajuli and Charlie Baxter <bhawani+charlie@>
2016-09-06 10:12:00 -0700

    initial file structure, display items in database on Items route

---
+++ src/todoList/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using todoList.Models;
+
+namespace todoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+    }
+}
